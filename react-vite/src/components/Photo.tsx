68094b5265056d2c5209acccb9dd161974b348f8
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-08-31 21:44:09 -0300

    Add files via upload

---
+++ react-vite/src/components/Photo.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from 'react';
+
+type Props = {
+    legend: string;
+    children: ReactNode;
+}
+
+export const Photo = ({ legend, children }: Props) => {
+    return (
+        <>
+            {children}
+            <p>{legend}</p>
+        </>
+    );
+}
