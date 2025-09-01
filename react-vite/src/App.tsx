9293ef33a95d7ea5b3b406e9c37ee4ceb3960925
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-09-01 11:37:48 -0300

    Update App.tsx

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,23 +1,11 @@
+// Terá um número inicial e toda vez que clicar no botão, o contador soma +1
 import { useState } from 'react';
-import { Header } from './components/header'
-import { Photo } from './components/Photo'
 
 const App = () => {
-  const [name, setName] = useState('Taynan');
-
-  const buttonClick = () => {
-    setName('Livia');
-  }
-
+  
   return (
     <div>
-      <Header title = 'Só os mais lindos' />
       
-      Olá {name}.
-      <button onClick = {buttonClick}>Clique aqui</button>
-      <Photo legend = 'Google'>
-        <img src = 'https://www.google.com.br/google.jpg'/>
-      </Photo>
     </div>
   );
   
