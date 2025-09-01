87d4c6e6cf54d32daf7107dd6cac1a43b81bd0a6
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-08-31 21:44:50 -0300

    Update App.tsx

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,10 +1,18 @@
+import { Header } from './components/header'
+import { Photo } from './components/Photo'
+ 
 const App = () => {
-  let link: string = 'https://google.com';
 
   return (
     <div>
-      Link para o site:<br/>
-      <a href = {link}>Clique aqui para acesar</a>
+      <Header title='Este é um exemplo'/>
+      <Header title='Outro Texto'/><br/>
+
+      Olá Mundo<br/>
+
+      <Photo legend='Google'>
+        <img src="https://www.google.com.br/google.jpg" />  
+      </Photo>
     </div>
   );
   
