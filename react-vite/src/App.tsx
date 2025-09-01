b3d48717447d35cbfd95d13d370c8f3e2460736f
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-09-01 11:35:36 -0300

    Update App.tsx

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,17 +1,22 @@
+import { useState } from 'react';
 import { Header } from './components/header'
 import { Photo } from './components/Photo'
- 
+
 const App = () => {
+  const [name, setName] = useState('Taynan');
+
+  const buttonClick = () => {
+    setName('Livia');
+  }
 
   return (
     <div>
-      <Header title='Este é um exemplo'/>
-      <Header title='Outro Texto'/><br/>
-
-      Olá Mundo<br/>
-
-      <Photo legend='Google'>
-        <img src="https://www.google.com.br/google.jpg" />  
+      <Header title = 'Só os mais lindos' />
+      
+      Olá {name}.
+      <button onClick = {buttonClick}>Clique aqui</button>
+      <Photo legend = 'Google'>
+        <img src = 'https://www.google.com.br/google.jpg'/>
       </Photo>
     </div>
   );
