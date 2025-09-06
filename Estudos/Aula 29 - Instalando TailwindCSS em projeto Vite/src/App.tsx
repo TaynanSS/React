3df69184b7684f3b8de719a10920d4dcf15f5fb9
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-09-05 21:29:18 -0300

    Add files via upload

---
+++ Estudos/Aula 29 - Instalando TailwindCSS em projeto Vite/src/App.tsx
@@ -0,0 +1,11 @@
+// Pra funcionar precisa instalar o NPM com comando: npm install
+
+const App = () => {
+
+  return (
+    <div className='bg-orange-500'>...</div>
+  );
+  
+}
+
+export default App; 
