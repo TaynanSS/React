e2b03662418f5d3fe24108b37a83123e1cf024dc
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-09-06 12:59:16 -0300

    Add files via upload

---
+++ Estudos/Aula 29 30 e 31 - Usando TailwindCSS/src/App.tsx
@@ -0,0 +1,11 @@
+// Pra funcionar precisa instalar o NPM com comando: npm install
+
+const App = () => {
+
+  return (
+    <div className='bg-blue-500 text-white p-4 font-bold'>Texto Qualquer Aqui</div>
+  );
+  
+}
+
+export default App; 
