5c8021bbeb4842c67d06c478eba633856677316a
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-09-16 17:58:37 -0300

    Add files via upload

---
+++ Estudos/Aula 62 - Introdução aos Reducers 1/src/App.tsx
@@ -0,0 +1,38 @@
+import { useReducer } from 'react';
+
+type reducerState = {
+  count: number;
+}
+type reducerAction = {
+  type: string;
+}
+
+const initialState = { count: 0 };
+const reducer = (state: reducerState, action: reducerAction) => { // aqui ele vai SEMPRE receber dois parâmetros. 1°: Valor atual do reducer || 2°: Qual ação quer executar. OS DOIS PRECISAM SER TIPADOS -> Type
+  switch(action.type) {
+    case 'ADD':
+      return {...state, count: state.count + 1};
+    break;
+    case 'DEL':
+      return {...state, count: state.count - 1};
+    break;
+    case 'RESET':
+      return initialState;
+    break;
+  }
+  return state;
+} // Essa função reducer funciona da seguinte forma:
+// Ele recebe o state, farei as alterações se baseando no action no state, e no final ele vai querer retornar como ficou o state.
+
+
+const App = () => { // useReducer recebe dois parâmetros, o 1° é a função e o 2° é o valor inicial, igual ao useState
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className='p-5'>
+
+    </div>
+  ); 
+}
+
+export default App; 
