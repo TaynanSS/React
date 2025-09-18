9b3d3216d885dc78598f9cd660d313d98e436b66
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-09-18 17:05:22 -0300

    Add files via upload

---
+++ Estudos/p1 - Project_DevMemory - Criando a Estrutura/src/App.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const infoArea = styled.div``;
+
+export const Logo = styled.a``;
+
+export const info = styled.div``;
+
+export const gridArea = styled.div``;
