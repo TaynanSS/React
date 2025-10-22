afb164b1b2c6900bf69773c510f97b2e8236c139
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-10-22 12:01:05 -0300

    Add files via upload

---
+++ Estudos/Aula 0101 - Criando context com Reducers 2/src/pages/SignUp.tsx
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import { Context } from '../contexts/Context';
+
+
+export const SignUp = () => {
+
+    const { name, age } = useContext(Context);
+
+    return (
+        <div>
+            Tela SignUp de {name} que tem {age} anos.
+            <br />
+            <Link to='/exibir'>Ir para ShowData</Link>
+        </div>
+    )
+}
