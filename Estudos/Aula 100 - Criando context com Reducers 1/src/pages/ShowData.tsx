6777daf4c06f468f1b5088b8aee9fae26a3e2b99
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-10-22 11:59:26 -0300

    Add files via upload

--- Estudos/Aula 100 - Criando context com Reducers 1/src/pages/ShowData.tsx
+++ Estudos/Aula 100 - Criando context com Reducers 1/src/pages/ShowData.tsx
@@ -1,15 +1,13 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
-import { Context } from '../contexts/Context';
+
 
 export const ShowData = () => {
 
-    const { name } = useContext(Context);
-
     return (
         <div>
-            Tela ShowData de {name}
+            Tela ShowData de 
             <br />
             <Link to='/' >Voltar para SignUp</Link>
         </div>
